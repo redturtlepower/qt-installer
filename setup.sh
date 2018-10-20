106503a71a48886766c5455992dc44f0a2e45682
Thomas Rusche <thomas.rusche@gmail.com>
2018-10-20 15:37:30 +0200

    changed prefix from sh to bash

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 mkdir generated
 mkdir installers
-sh setup/generate-qt-prefix.sh
-sh setup/generate-qtinstaller-script.sh
-sh setup/generate-provision-script.sh
-sh generated/provision.sh
+bash setup/generate-qt-prefix.sh
+bash setup/generate-qtinstaller-script.sh
+bash setup/generate-provision-script.sh
+bash generated/provision.sh
