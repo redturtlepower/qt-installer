dabf0a1887da40aebb3a5c99952f727635c48e8b
thomas <thomas.rusche@gmail.com>
2018-11-23 23:18:08 +0100

    Check if Qt is already installed

--- setup/generate-provision-script.sh
+++ setup/generate-provision-script.sh
@@ -1,1 +1,4 @@
 sed "s/__QT_VERSION__/`cat QT_VERSION.txt`/" setup/provision.sh.template > generated/provision.sh
+
+# Use fixed user name 'jenkins':
+sed -i "s/__USER_NAME__/`echo jenkins`/g" generated/provision.sh
